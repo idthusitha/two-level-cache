d2864c203032df3946c2f9335e133af62bdfefbb
thusitha <thusitha@thusitha-Asus>
2020-05-29 00:50:01 +0530

    initial commit

---
+++ src/com/cache/TwoLevelCache.java
@@ -0,0 +1,10 @@
+package com.cache;
+
+public class TwoLevelCache {
+
+	public static void main(String[] args) {
+		System.out.println("TwoLevelCache starting ..........................");
+
+	}
+
+}
